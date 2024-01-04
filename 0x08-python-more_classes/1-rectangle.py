e77bb33ff0858518ebb8c1e79f8c65838c077748
Ndege <kamothowilliam2@gmail.com>
2024-01-04 03:14:47 -0800

    task 1 update

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,5 +1,26 @@
 #!/usr/bin/python3
+"""
+Module 2-rectangle
+Contains class Rectangle
+with private attribute width and height
+"""
+
+
 class Rectangle:
+    """
+    Defines class rectangle with private attribute width and height
+
+    Args:
+        width (int): width
+        height (int): height
+
+    Functions:
+        __init__(self, width, height)
+        width(self)
+        width(self, value)
+        height(self)
+        height(self, value)
+    """
     def __init__(self, width=0, height=0):
         """ Initialize rectangles """
         self.width = width
