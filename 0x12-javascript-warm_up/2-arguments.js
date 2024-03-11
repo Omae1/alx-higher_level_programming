9d8a9e3a1e6c159d338674ff2f8a0829a716305a
Ndege <kamothowilliam2@gmail.com>
2024-03-11 03:14:01 -0700

    update no.2

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-if (process.argv.length > 3) {
+if (process.argv.length >= 4) {
   console.log('Arguments found');
 } else if (process.argv.length === 3) {
   console.log('Argument found');
